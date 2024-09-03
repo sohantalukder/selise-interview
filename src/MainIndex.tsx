1f71e065c888198adea7490f143f1687adf29ac9
sohanorangetoolz <sohan@orangetoolz.com>
2024-09-03 17:31:39 +0600

    remove pacakge and add redme

--- src/MainIndex.tsx
+++ src/MainIndex.tsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
-import Toast from 'react-native-toast-message';
 import {GestureHandlerRootView} from 'react-native-gesture-handler';
 import {globalStyles} from './assets/styles/global.style.asset';
 import RouterIndex from './routes/RouterIndex.routes';
 import BottomSheetIndex from './components/bottom-sheet';
-import toastConfig from './packages/toast/toastConfig';
 import {Provider} from 'react-redux';
 import configStore from './redux/store';
 
@@ -18,7 +16,6 @@ const MainIndex = () => {
           <NavigationContainer>
             <RouterIndex />
             <BottomSheetIndex />
-            <Toast config={toastConfig} position="bottom" />
           </NavigationContainer>
         </GestureHandlerRootView>
       </Provider>
